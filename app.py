d1d98a28b43139dd6256a236da5324598d57afcf
Chandrashekhar <chandrashekhar130697@gmail.com>
2024-01-09 11:45:31 +0530

    fix scaler.pkl file

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict_api():
     data = request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scalar.fit_transform(np.array(list(data.values())).reshape(1,-1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
     output= regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
@@ -26,11 +26,12 @@ def predict_api():
 @app.route('/predict', methods=['POST'])
 def predict():
     data = [float(request.form[x]) for x in request.form]
-    final_input = scalar.fit_transform(np.array(data).reshape(1, -1))
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
     output = regmodel.predict(final_input)[0]
     return render_template("home.html", prediction_text="The house price prediction is : {}.".format(output))
 
 
 if __name__=="__main__":
     app.run(debug=True)
+
 
